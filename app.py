5071e87714f8cc77c6467b91facb5b8773d65210
Lý Phúc Thành <20521916@gm.uit.edu.vn>
2024-12-23 00:14:33 +0700

    initial commit

--- app.py
+++ app.py
@@ -1,12 +1,30 @@
-import essentia
-import essentia.standard as es
+import os
+from flask import Flask, request, make_response, Response, Blueprint
+import json
+import time
+from dotenv import load_dotenv
+from flask_cors import CORS
 
-def extract_music_features(audio_path):
-    extractor = es.MusicExtractor()
-    features = extractor(audio_path)
-    return features
+class App:
+    def __init__(self):
+        self.app = Flask(__name__)
+        self.app_bp = Blueprint('app', __name__)
+        self._setup_routes()
+        self._initialize_app()
+        CORS(self.app)
+    
+    def _initialize_app(self):
+        with self.app.app_context():
+            load_dotenv()
+            if self.app.name not in self.app.blueprints:
+                self.app.register_blueprint(self.app_bp, url_prefix='/uit/dsp')
 
-if __name__ == "__main__":
-    audio_file = "TraiHoVubeat.mp3"
-    features = extract_music_features(audio_file)
-    print(features)
+    def _setup_routes(self):
+        @self.app_bp.route('', methods=['POST'])
+        def preprocessing():
+           return
+    
+    
+    def run(self, port):
+        self.app.run(port=port, debug=True)
+
